cba6c76cac85a2e261fae450b0355516ef855eac
OlhaHala <olhahaladzhian@gmail.com>
2022-07-31 13:05:08 +0300

    Change icon

--- src/app.js
+++ src/app.js
@@ -29,15 +29,21 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector(`#humidity`);
   let windElement = document.querySelector(`#wind`);
   let dateElement = document.querySelector(`#date`);
+  let iconElement = document.querySelector(`#icon`);
   tempElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
+let city = "Lisbon";
 let apiKey = "6787254059ff334afbc56a41b925e8c5";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Odessa&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
